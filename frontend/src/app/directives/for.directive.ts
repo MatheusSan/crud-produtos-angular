96ddd4c55b4dc48d38061a1c4de06eeb41751102
MatheusSan <matheus08santos@hotmail.com>
2022-01-25 17:59:55 -0300

    feat: navigations and directives

---
+++ frontend/src/app/directives/for.directive.ts
@@ -0,0 +1,21 @@
+import { Directive, Input, OnInit, TemplateRef, ViewContainerRef } from '@angular/core';
+
+@Directive({
+  selector: '[myFor]'
+})
+export class ForDirective implements OnInit {
+
+  @Input("myForEm") numbers: number[] = [];
+  // @Input('myForUsando') texto: string //Segundo argumento
+
+  //A chamada dessa diretiva está no footer
+
+  constructor(private container: ViewContainerRef, private template: TemplateRef<any>) { }
+
+  ngOnInit(): void {
+    for(let number of this.numbers){
+      this.container.createEmbeddedView(this.template, {$implicit: number})
+    }
+  }
+
+}
